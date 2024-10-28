fc9819f0afc04571e9ad72d0b2cb09a8220ca89e
ambaqinejad <ambaqinejad@gamil.com>
2024-10-28 23:09:41 +0330

    wip

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        BeanFactory factory = new ClassPathXmlApplicationContext("applicationContext.xml");
+        Alien a = factory.getBean("alien", Alien.class);
+        a.code();
     }
 }
