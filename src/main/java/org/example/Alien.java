d959e7831daea832a6796c19dcdc595a0ff52de5
ambaqinejad <ambaqinejad@gamil.com>
2024-10-29 21:35:59 +0330

    Singleton Bean

--- src/main/java/org/example/Alien.java
+++ src/main/java/org/example/Alien.java
@@ -1,6 +1,10 @@
 package org.example;
 
 public class Alien {
+    int age;
+    public Alien() {
+        System.out.println("Alien object created");
+    }
     public void code() {
         System.out.println("I'm coding ...");
     }
--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,6 +1,7 @@
 package org.example;
 
-import org.springframework.beans.factory.BeanFactory;
+//import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
@@ -11,8 +12,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        BeanFactory factory = new ClassPathXmlApplicationContext("applicationContext.xml");
+//        BeanFactory factory = new ClassPathXmlApplicationContext("applicationContext.xml");
+        ApplicationContext factory = new ClassPathXmlApplicationContext("applicationContext.xml");
         Alien a = factory.getBean("alien", Alien.class);
         a.code();
+        a.age = 27;
+        System.out.println(a.age);
+        Alien b = factory.getBean("alien", Alien.class);
+        System.out.println(b.age);
     }
 }
